5825809786ecad0a619842dd810b7820979d0744
muzammil-git <syedmuzammil67@gmail.com>
2021-08-12 00:54:59 +0500

    initial commit

--- 10.List.py
+++ 10.List.py
@@ -1,1 +1,36 @@
-fruits = ["apple", "banana"]
+fruits = ["apple", "banana","Cherry"]
+fruits[1] = "Orange"    #Replaces 1th index element
+fruits.pop() #Removes the element at the n-1 index
+fruits.append("Pear") #Adds Element
+print(fruits)
+
+exported_fruits = fruits
+imported_fruits = ["KIWI", "Pumpkin","BlueBerry"]
+
+print(f"Exported Fruits: {exported_fruits}")
+print(f"Imported Fruits: {imported_fruits}")
+
+#Concatenating List
+print(f"All Fruits: {imported_fruits+exported_fruits}")
+
+
+#Iterating fruits
+for i in range(len(fruits)):
+    print(fruits[i])
+
+#Making exception just to test
+try:
+    print(fruits[1000])
+except Exception:
+    print("Something went wrong")
+
+
+#List Guess Game
+
+colors = ["Red", "Yellow", "Green"]
+guess = input("Guess the color: ")
+
+if guess in colors:
+    print(f"You are right its {guess}")
+else:
+    print("You are wrong")
